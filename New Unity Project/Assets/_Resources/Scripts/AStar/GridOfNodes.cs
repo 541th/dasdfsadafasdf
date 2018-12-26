5d3dd67cedec67cad392160986de71238f7c6da7
moort <rdliteling@gmai.com>
2018-12-26 18:38:04 +0300

    промежуточный коммит 0

---
+++ New Unity Project/Assets/_Resources/Scripts/AStar/GridOfNodes.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridOfNodes : MonoBehaviour
+{
+    public GridsNode[,] nodes;
+
+    public Vector2 mapSize;
+    public bool drawGizmosW, drawGizmosNW;
+    public bool nodeIsCreated;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        createGrid();
+    }
+
+    Vector2 mainMapPos;
+
+    private void Start()
+    {
+        mainMapPos = transform.position;
+        //mainMapPos = FindObjectOfType<Tiled2Unity.TiledMap>().transform.position;
+        createGrid();
+    }
+
+    private void Update()
+    {
+        //mainMapPos = FindObjectOfType<Tiled2Unity.TiledMap>().transform.position;
+        createGrid();
+    }
+
+    public int MaxSize
+    {
+        get
+        {
+            return (int)mapSize.x * (int)mapSize.y;
+        }
+    }
+
+    int idMaxX, idMaxY;
+    void createGrid()
+    {
+        mapSize = GetComponent<BoxCollider2D>().size;
+        nodes = new GridsNode[(int)mapSize.x * 2, (int)mapSize.y * 2];
+
+        float x = GetComponent<BoxCollider2D>().transform.position.x - (int)mapSize.x / 2;
+        float y = GetComponent<BoxCollider2D>().transform.position.y - (int)mapSize.y / 2;
+        int idX = 0, idY = 0;
+
+        while (x < GetComponent<BoxCollider2D>().transform.position.x + (int)mapSize.x / 2)
+        {
+            while (y < GetComponent<BoxCollider2D>().transform.position.y + (int)mapSize.y / 2)
+            {
+                nodes[idX, idY] = new GridsNode();
+                nodes[idX, idY].pos = new Vector2(x, y);
+                nodes[idX, idY].idX = idX;
+                nodes[idX, idY].idY = idY;
+
+                nodes[idX, idY].walkable = isWalkable(new Vector2(x, y));
+
+                idY++;
+                y += 0.5f;
+            }
+
+            x += 0.5f;
+            y = GetComponent<BoxCollider2D>().transform.position.y - (int)mapSize.y / 2;
+
+            idX++;
+
+            idMaxY = idY;
+            idY = 0;
+        }
+
+        idMaxX = idX;
+
+        nodeIsCreated = true;
+    }
+
+    public bool isWalkable(Vector2 point)
+    {
+        return !(Physics2D.BoxCast(new Vector2(point.x, point.y + 0.1f), new Vector2(0.1f, 0.1f), 0, new Vector2(0, 0)).collider
+                    && (!Physics2D.BoxCast(new Vector2(point.x, point.y + 0.1f), new Vector2(0.1f, 0.1f), 0, new Vector2(0, 0)).collider.isTrigger));
+    }
+
+    public List<GridsNode> GetNeighboursOf(GridsNode node, string whoCalls)
+    {
+        List<GridsNode> n = new List<GridsNode>();
+        int _i = 0, _j = 0;
+
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+            {
+                _i = i; _j = j;
+
+                if (whoCalls == "Enemy")
+                    if (i == 0 && j == 0) continue;
+
+                if (i < 0) _i = i * -1;
+                if (j < 0) _j = j * -1;
+
+                if (_i == _j && whoCalls != "Enemy") continue;
+
+                int x = node.idX + i;
+                int y = node.idY + j;
+
+                if (x >= 0 && x < idMaxX && y >= 0 && y < idMaxY)
+                    n.Add(nodes[x, y]);
+            }
+
+        return n;
+    }
+
+    public GridsNode GetNodeByPos(Vector2 pos)
+    {
+        pos -= mainMapPos;
+
+        int x = (int)(pos.x * 2);
+        int y = idMaxY - (int)(pos.y * 2) * -1;
+
+        return nodes[x, y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (nodeIsCreated && (drawGizmosNW || drawGizmosW))
+        {
+            for (int x = 0; x < idMaxX; x++)
+                for (int y = 0; y < idMaxY; y++)
+                {
+                    if (!nodes[x, y].walkable && drawGizmosNW)
+                    {
+                        Gizmos.color = Color.red;
+
+                        Gizmos.DrawCube(new Vector3(nodes[x, y].pos.x, nodes[x, y].pos.y, 0), new Vector3(0.25f, 0.25f, 0.25f));
+                    }
+
+                    if (nodes[x, y].walkable && drawGizmosW)
+                    {
+                        Gizmos.color = Color.green;
+
+                        Gizmos.DrawCube(new Vector3(nodes[x, y].pos.x, nodes[x, y].pos.y, 0), new Vector3(0.25f, 0.25f, 0.25f));
+                    }
+                }
+        }
+    }
+}
+
+public class GridsNode : IHeapItem<GridsNode>
+{
+    public int idX, idY;
+    public Vector2 pos;
+    public bool walkable;
+    public GridsNode cameFrom;
+    int heapIndex;
+
+    public int H, G;
+
+    public int F
+    {
+        get
+        {
+            return H + G;
+        }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(GridsNode nodeToCompare)
+    {
+        int compare = F.CompareTo(nodeToCompare.F);
+
+        if (compare == 0) compare = H.CompareTo(nodeToCompare.H);
+
+        return -compare;
+    }
+}
+
---
+++ New Unity Project/Assets/_Resources/Scripts/AStar/Heap.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heap <T> where T : IHeapItem<T> {
+
+    T[] items;
+    int curItemCount;
+
+    public Heap (int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+    public void add(T item)
+    {
+        item.HeapIndex = curItemCount;
+        items[curItemCount] = item;
+        sortUp(item);
+        curItemCount++;
+    }
+
+    public T removeFirst()
+    {
+        T first = items[0];
+        curItemCount--;
+        items[0] = items[curItemCount];
+        items[0].HeapIndex = 0;
+        sortDown(items[0]);
+        return first;
+    }
+
+    public void updateItem(T item)
+    {
+        sortUp(item);
+    }
+
+    public int Count
+    {
+        get
+        {
+            return curItemCount;
+        }
+    }
+
+    public bool contains(T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    void sortDown(T item)
+    {
+        while (true)
+        {
+            int childIndexLeft = item.HeapIndex * 2 + 1;
+            int childIndexRight = item.HeapIndex * 2 + 2;
+            int swapIndex = 0;
+
+            if (childIndexLeft < curItemCount)
+            {
+                swapIndex = childIndexLeft;
+
+                if (childIndexRight < curItemCount)
+                    if (items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+                        swapIndex = childIndexRight;
+
+                if (item.CompareTo(items[swapIndex]) < 0)
+                    swap(item, items[swapIndex]);
+                else return;
+            }
+            else return;
+        }
+    }
+
+    void sortUp(T item)
+    {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+
+        while(true)
+        {
+            T parentItem = items[parentIndex];
+            if (item.CompareTo(parentItem) > 0)
+            {
+                swap(item, parentItem);
+            }
+            else
+            {
+                break;
+            }
+
+            parentIndex = (item.HeapIndex - 1) / 2;
+        }
+    }
+
+    void swap(T item1, T item2)
+    {
+        items[item1.HeapIndex] = item2;
+        items[item2.HeapIndex] = item1;
+        int item1Index = item1.HeapIndex;
+        item1.HeapIndex = item2.HeapIndex;
+        item2.HeapIndex = item1Index;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex
+    {
+        get;
+        set;
+    }
+}

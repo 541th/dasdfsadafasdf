0be19c76e32f25d850e8a38a1452656ac64797f9
moort <rdliteling@gmai.com>
2018-12-27 22:12:14 +0300

    Можно уже делать город

--- New Unity Project/Assets/_Resources/Scripts/Enemy/Enemy_0.cs
+++ New Unity Project/Assets/_Resources/Scripts/Enemy/Enemy_0.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Enemy_0 : MonoBehaviour
 {
     Transform _t;
     public LayerMask mask;
@@ -20,7 +20,7 @@ public class EnemyMovement : MonoBehaviour
     float delay, delta;
     GridOfNodes gon;
 
-    public enum State { walk, startAttack, attack };
+    public enum State { walk, attack };
     public State curState;
 
     public Vector2 dir;
@@ -29,10 +29,11 @@ public class EnemyMovement : MonoBehaviour
 
     float walkCounter;
     float waitCounter;
-    float attackCounter = 5;
 
     bool isWalking;
     bool gotDir;
+
+    Vector2 playerRandomPoint;
 
     void Start()
     {
@@ -55,14 +56,8 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        if (player != null && curState == State.walk)
-        {
-            setSA();
-        }
-
         if (curState == State.walk)
         {
-            _a.SetBool("Attack", false);
             if (isWalking)
             {
                 if (!gotDir)
@@ -70,14 +65,12 @@ public class EnemyMovement : MonoBehaviour
                     dir = AI.chooseDir();
                     gotDir = true;
 
-                    //setCollider(false);
-                }
+                    RaycastHit2D ray = Physics2D.Raycast(_t.position, dir, 10, mask);
 
-                RaycastHit2D ray = Physics2D.Raycast(_t.position, dir, 10, mask);
-
-                if (ray.collider != null && !ray.collider.isTrigger)
-                {
-                    dir = AI.chooseDirectionWithException(AI.getDirByVector(dir));
+                    if (ray.collider != null && !ray.collider.isTrigger)
+                    {
+                        dir = AI.chooseDirectionWithException(AI.getDirByVector(dir));
+                    }
                 }
 
                 walkCounter -= Time.deltaTime;
@@ -100,52 +93,74 @@ public class EnemyMovement : MonoBehaviour
                 if (waitCounter <= 0) startWalking();
             }
         }
-        else if (curState == State.startAttack)
-        {
-            Vector2 dir = player.transform.position - _t.position;
-            /*
-            float angle = Mathf.Atan2(dir.y, dir.x) * 180 / Mathf.PI - 90;
-
-            if (angle <= 41 && angle > -57) AI.setDirTo(ref this.dir, new Vector2(0, 1));
-            else
-            if (angle <= -57 && angle > -126) AI.setDirTo(ref this.dir, new Vector2(1, 0));
-            else
-            if (angle <= -126 && angle > -230) AI.setDirTo(ref this.dir, new Vector2(0, -1));
-            else
-            if (angle <= -230 || angle > 41) AI.setDirTo(ref this.dir, new Vector2(-1, 0));
-            */
-            attackCounter -= Time.deltaTime;
-
-            if (attackCounter < 0) curState = State.attack;
-        }
         else if (curState == State.attack)
         {
-            _a.SetBool("Run", true);
             delay += Time.deltaTime;
+            delta -= Time.deltaTime;
+
+            if ((Vector2.Distance(_t.position, player.transform.position) < 2))
+            {
+                _a.SetTrigger("Attack");
+                return;
+            }
+
+            if (delta <= 0)
+            {
+                //if (Vector2.Distance(transform.position, ((targets.Count == 0) ? player.transform.position : targets[0].transform.position)) > 2)
+                {
+                    playerRandomPoint = new Vector2(player.transform.position.x + Random.Range(-2.1f, 2.1f), 
+                        player.transform.position.y + Random.Range(-2.1f, 2.1f));
+
+                    if (gon.GetNodeByPos(playerRandomPoint) != null)
+                        while (!gon.GetNodeByPos(playerRandomPoint).walkable)
+                        {
+                            playerRandomPoint = new Vector2(player.transform.position.x + Random.Range(-2.1f, 2.1f), 
+                                player.transform.position.y + Random.Range(-2.1f, 2.1f));
+                        }
+                    else
+                    {
+                        playerRandomPoint = player.transform.position;
+                    }
+
+                    playerRandomPoint = gon.GetNodeByPos(playerRandomPoint).pos;
+                }
+
+                delta = 2;
+            }
+
+            if (Vector2.SqrMagnitude(transform.position - player.transform.position) < 20)
+            {
+                playerRandomPoint = player.transform.position;
+            }
 
             if (delay >= Random.Range(0, 0.3f))//Random.Range(0.8f, 1.2f))
             {
-                path = AI.setDestination(_t.position, player.transform.position);
+                path = AI.setDestination(transform.position, playerRandomPoint);
                 points = path.Count - 1;
                 delay = 0;
             }
-            
-            if ((Vector2.Distance(_t.position, player.transform.position) < 2) && !_a.GetBool("Attack"))
-            {
-                attack();
-                //setCollider(true);
-                return;
+
+        //if (delay >= Random.Range(0, 0.3f))//Random.Range(0.8f, 1.2f))
+        {
+
+                /*
+                if (Vector2.SqrMagnitude(_t.position - player.transform.position) < 30)
+                {
+                }
+                else
+                {
+                    path = AI.setDestination(_t.position, player.transform.position + new Vector3(Random.Range(-2.1f, 2), Random.Range(-2.1f, 2), 0));
+                }
+
+                path = AI.setDestination(_t.position, playerRandomPoint);
+                points = path.Count - 1;
+                delay = 0;
+                */
             }
 
             if (path != null)
                 if (path.Count != 0)
                 {
-                    if (points < 0)
-                    {
-                        attack();
-                        return;
-                    }
-
                     _t.position = Vector2.MoveTowards(_t.position, path[points], ms * Time.deltaTime);
 
                     if (Vector2.Distance(_t.position, path[points]) < 0.01f && points != 0)
@@ -174,57 +189,6 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    void attack()
-    {
-        path.Clear();
-        _a.SetBool("Attack", true);
-
-        /*
-        Vector2 dirToPlayer = (Vector2)player.transform.position - (Vector2)transform.position;
-        float angle = Mathf.Atan2(dirToPlayer.y, dirToPlayer.x) * 180 / Mathf.PI - 90;
-
-        if (angle <= 41 && angle > -57) AI.setDirTo(ref dir, new Vector2(0, 1));
-        else
-        if (angle <= -57 && angle > -126) AI.setDirTo(ref dir, new Vector2(1, 0));
-        else
-        if (angle <= -126 && angle > -230) AI.setDirTo(ref dir, new Vector2(0, -1));
-        else
-        if (angle <= -230 || angle > 41) AI.setDirTo(ref dir, new Vector2(-1, 0));
-        */
-        Invoke("setSA", 1f);
-    }
-
-    void setSA()
-    {
-        if (attackCounter <= 0) attackCounter = Random.Range(0, 2);
-        curState = State.startAttack;
-        _a.SetBool("Walk", false);
-        _a.SetBool("Run", false);
-        _a.SetBool("Attack", false);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.CompareTag("Player"))
-        {
-            _a.SetBool("Attack", true);
-            _a.SetBool("Run", false);
-            attack();
-            return;
-        }
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.transform.CompareTag("Player"))
-        {
-            if (!_a.GetBool("Attack") && curState == State.attack && _a.GetBool("Run"))
-            {
-                attack();
-            }
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -232,10 +196,23 @@ public class EnemyMovement : MonoBehaviour
             if (_a == null) _a = GetComponent<Animator>();
 
             player = collision.transform.gameObject;
-            attackCounter = Random.Range(2, 5);
-            curState = State.startAttack;
-            _a.SetBool("Walk", false);
+
+            Invoke("startAttack", Random.Range(0.4f, 1));
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player") && collision.transform.GetComponent<PlayerMovement>().isGlide)
+        {
+            GetComponentInChildren<EnemyHP>().toDamage(Random.Range(4, 10), true);
+        }
+
+    }
+
+    void startAttack()
+    {
+        curState = State.attack;
     }
 
     void startWalking()
---
+++ New Unity Project/Assets/_Resources/Scripts/Other/FloatingNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingNumbers : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position += new Vector3(Random.Range(-0.4f, 0.4f), 0);
+        Destroy(transform.parent.gameObject, 1);
+    }
+
+    public void setText(string value)
+    {
+        GetComponent<Text>().text = value;
+    }
+
+    void Update()
+    {
+        transform.parent.position += new Vector3(0, Time.deltaTime);
+        GetComponent<Text>().color -= new Color(0, 0, 0, Time.deltaTime);
+    }
+}

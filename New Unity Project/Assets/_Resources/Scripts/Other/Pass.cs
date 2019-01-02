083500527bc5b2aea6bd6c4c03b063c8ad99ec66
moort <rdliteling@gmai.com>
2019-01-02 17:02:37 +0300

    Проход и границы карты

---
+++ New Unity Project/Assets/_Resources/Scripts/Other/Pass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pass : MonoBehaviour
+{
+    [SerializeField] Transform _0, _1;
+    public void pass0()
+    {
+        GameObject.Find("Player").transform.position = _1.position;
+    }
+
+    public void pass1()
+    {
+        GameObject.Find("Player").transform.position = _0.position;
+    }
+}
---
+++ New Unity Project/Assets/_Resources/Scripts/Other/PassTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}

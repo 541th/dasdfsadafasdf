107aa52807c85f0281ad1373d5031d4799051c6b
moort <rdliteling@gmai.com>
2019-01-07 20:20:53 +0300

    Давилка и шипы

---
+++ New Unity Project/Assets/_Resources/Scripts/Other/PusherController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusherController : MonoBehaviour
+{
+    [SerializeField] float startTimer, start, end;
+    Transform _t;
+
+    void Start()
+    {
+        _t = transform;
+        StartCoroutine(pusherEvent());
+        start = _t.localPosition.y;
+    }
+
+    IEnumerator pusherEvent()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(6 + startTimer);
+
+            _t.GetChild(0).gameObject.SetActive(true);
+
+            while (_t.localPosition.y < end)
+            {
+                _t.localPosition += new Vector3(0, Time.deltaTime * 30, 0);
+                yield return null;
+            }
+
+            _t.localPosition = new Vector2(_t.localPosition.x, end);
+
+            yield return new WaitForSeconds(1);
+
+            _t.GetChild(0).gameObject.SetActive(false);
+
+            while (_t.localPosition.y > start)
+            {
+                _t.localPosition -= new Vector3(0, Time.deltaTime * 4, 0);
+                yield return null;
+            }
+
+            _t.localPosition = new Vector2(_t.localPosition.x, start);
+        }
+    }
+}
--- New Unity Project/Assets/_Resources/Scripts/Other/SortingOrderAtStart.cs
+++ New Unity Project/Assets/_Resources/Scripts/Other/SortingOrderAtStart.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class SortingOrderAtStart : MonoBehaviour
 {
-    [SerializeField] bool isGrass;
+    [SerializeField] bool isGrass, isTorch, isSpikes;
+    [SerializeField] float startTimer;
 
     void Start()
     {
+        if (isSpikes)
+        {
+            GetComponent<Animator>().enabled = false;
+            StartCoroutine(showSpikes());
+            return;
+        }
+
+        if (isTorch)
+        {
+            GetComponent<Animator>().speed = Random.Range(0.8f, 1.6f);
+            return;
+        }
+
         if (isGrass)
             GetComponent<Animator>().speed = Random.Range(0.4f, 1.1f);
         GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * SortingOrder.mult);
+    }
+
+    IEnumerator showSpikes()
+    {
+        yield return new WaitForSeconds(startTimer);
+        GetComponent<Animator>().enabled = true;
     }
 }

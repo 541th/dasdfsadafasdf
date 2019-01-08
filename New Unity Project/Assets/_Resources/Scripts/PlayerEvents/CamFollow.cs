61ed3c34ae3046ee72cde2b321a5c3dc81330500
moort <rdliteling@gmai.com>
2019-01-08 14:43:24 +0300

    Стрелка при стрельбе

--- New Unity Project/Assets/_Resources/Scripts/PlayerEvents/CamFollow.cs
+++ New Unity Project/Assets/_Resources/Scripts/PlayerEvents/CamFollow.cs
@@ -6,6 +6,7 @@ using CnControls;
 public class CamFollow : MonoBehaviour
 {
     public GameObject followTarget;
+    Transform shotDir;
 
     private Vector3 targetPos;
     public float moveSpeed;
@@ -24,9 +25,10 @@ public class CamFollow : MonoBehaviour
             Destroy(gameObject);
         
         if (followTarget == null) followTarget = GameObject.Find("Player");
+        shotDir = followTarget.transform.GetChild(followTarget.transform.childCount - 1);
     }
     
-    float clampedX, clampedY;
+    float clampedX, clampedY, a_h, a_v;
     Vector2 delta;
     bool arrowShotShaking;
     // Update is called once per frame
@@ -34,11 +36,22 @@ public class CamFollow : MonoBehaviour
     {
         if (followTarget != null)
         {
+            a_h = CnInputManager.GetAxis("Attack_H") * 2;
+            a_v = CnInputManager.GetAxis("Attack_V") * 2;
             if (!arrowShotShaking)
-                delta = new Vector2(CnInputManager.GetAxis("Attack_H") * 2, CnInputManager.GetAxis("Attack_V") * 2);
+                delta = new Vector2(a_h, a_v);
             else
                 delta = Vector2.zero;
-            
+
+            if (a_h != 0 || a_v != 0)
+            {
+                shotDir.gameObject.SetActive(true);
+                shotDir.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(a_v, a_h) * 180 / Mathf.PI - 90);
+                shotDir.transform.localPosition = new Vector3(a_h, a_v, 1);
+            }
+            else
+                shotDir.gameObject.SetActive(false);
+
             targetPos = new Vector3(followTarget.transform.position.x + delta.x, followTarget.transform.position.y + delta.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
         }
